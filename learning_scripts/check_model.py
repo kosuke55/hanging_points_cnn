e990bad46eaf158614aa28dbe6fb7a824d2b75ff
Your Name <kosuke.tnp@gmail.com>
2020-05-23 16:18:40 +0900

    Update check model

--- learning_scripts/check_model.py
+++ learning_scripts/check_model.py
@@ -13,7 +13,7 @@ import sys
 # sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 # sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages')
 
-from utils.rois_tools import expand_roi, find_rois
+from utils.rois_tools import expand_box, find_rois
 
 
 config = {
@@ -44,51 +44,52 @@ for idx in range(1):
 
     # data_name = data_name.sort()[0]
 
-    depth = np.load(
-        os.path.join(data_path, "depth/",
-                     os.path.splitext(data_name)[0]) + ".npy").astype(np.float32) * 0.001
-    color = cv2.imread(os.path.join(data_path, "color/", data_name)).astype(np.float32)
+    # depth = np.load(
+    #     os.path.join(data_path, "depth/",
+    #                  os.path.splitext(data_name)[0]) + ".npy").astype(np.float32) * 0.001
+    # color = cv2.imread(os.path.join(data_path, "color/", data_name)).astype(np.float32)
 
-    confidence_gt_img = cv2.imread(
-        os.path.join(data_path, "heatmap/", data_name),
-        cv2.IMREAD_GRAYSCALE).astype(np.float32)
-    confidence_gt = confidence_gt_img / 255.
+    
+    # confidence_gt_img = cv2.imread(
+    #     os.path.join(data_path, "heatmap/", data_name),
+    #     cv2.IMREAD_GRAYSCALE).astype(np.float32)
+    # confidence_gt = confidence_gt_img / 255.
 
     model = HPNET(config).cuda()
-    model.load_state_dict(torch.load(pretrained_model), strict=False)
-    # image = torch.rand((4, 1, 256, 256)).cuda()
-    transform = transforms.Compose([
-        transforms.ToTensor()])
-    depth = transform(depth)[None, ...].cuda()
-    # print(depth.shape)
-    confidence, depth_and_rotation = model.forward(depth)
+    # model.load_state_dict(torch.load(pretrained_model), strict=False)
+    # # image = torch.rand((4, 1, 256, 256)).cuda()
+    # transform = transforms.Compose([
+    #     transforms.ToTensor()])
+    # depth = transform(depth)[None, ...].cuda()
+    # # print(depth.shape)
+    # confidence, depth_and_rotation = model.forward(depth)
 
-    confidence_gt = transform(confidence_gt)[None, ...].cuda()
-    gt_rois_list = find_rois(confidence_gt)
-    print('gt_rois', gt_rois_list)
-    print('rois', model.rois_list)
+    # confidence_gt = transform(confidence_gt)[None, ...].cuda()
+    # gt_rois_list = find_rois(confidence_gt)
+    # print('gt_rois', gt_rois_list)
+    # print('rois', model.rois_list)
 
-    print('confidence.shape', confidence.shape)
-    print('depth_and_rotation.shape', depth_and_rotation.shape)
+    # print('confidence.shape', confidence.shape)
+    # print('depth_and_rotation.shape', depth_and_rotation.shape)
 
-    print(model.rois_list)
-    confidence = confidence[0, 0, ...]
-    confidence = confidence.cpu().detach().numpy().copy() * 255
-    confidence = confidence.astype(np.uint8)
+    # print(model.rois_list)
+    # confidence = confidence[0, 0, ...]
+    # confidence = confidence.cpu().detach().numpy().copy() * 255
+    # confidence = confidence.astype(np.uint8)
 
-    confidence_bgr = cv2.cvtColor(confidence, cv2.COLOR_GRAY2BGR)
+    # confidence_bgr = cv2.cvtColor(confidence, cv2.COLOR_GRAY2BGR)
 
-    for rois in model.rois_list:
-        # print('--')
-        for roi in rois:
-            # print(roi)
-            confidence_bgr = cv2.rectangle(
-                confidence_bgr, (roi[0], roi[1]), (roi[2], roi[3]), (0, 255, 0), 3)
-            color = cv2.rectangle(
-                color, (roi[0], roi[1]), (roi[2], roi[3]), (0, 255, 0), 3)
+    # for rois in model.rois_list:
+    #     # print('--')
+    #     for roi in rois:
+    #         # print(roi)
+    #         confidence_bgr = cv2.rectangle(
+    #             confidence_bgr, (roi[0], roi[1]), (roi[2], roi[3]), (0, 255, 0), 3)
+    #         color = cv2.rectangle(
+    #             color, (roi[0], roi[1]), (roi[2], roi[3]), (0, 255, 0), 3)
 
-    cv2.imwrite('debug/confidence{:03}.png'.format(idx), confidence_bgr)
-    cv2.imwrite('debug/color{:03}.png'.format(idx), color)
-    # cv2.imwrite('confidence.png', confidence)
-
-    # summary(model, (1, 256, 256)) # summary(model,(channels,H,W))
+    # cv2.imwrite('debug/confidence{:03}.png'.format(idx), confidence_bgr)
+    # cv2.imwrite('debug/color{:03}.png'.format(idx), color)
+    # # cv2.imwrite('confidence.png', confidence)
+    
+    summary(model, (1, 256, 256)) # summary(model,(channels,H,W))
