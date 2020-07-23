76dd7bcf4ac34f975746eb869dd6bd0892d36f89
kosuke55 <kosuke.tnp@gmail.com>
2020-07-24 00:03:54 +0900

    Make package

---
+++ setup.py
@@ -0,0 +1,66 @@
+from __future__ import print_function
+
+import distutils.spawn
+import shlex
+import subprocess
+import sys
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "0.0.1"
+
+
+if sys.argv[-1] == "release":
+    if not distutils.spawn.find_executable("twine"):
+        print(
+            "Please install twine:\n\n\tpip install twine\n", file=sys.stderr,
+        )
+        sys.exit(1)
+
+    commands = [
+        "git tag v{:s}".format(version),
+        "git push origin master --tag",
+        "python setup.py sdist",
+        "twine upload dist/hanging_points_cnn-{:s}.tar.gz".format(
+            version),
+
+    ]
+    for cmd in commands:
+        print("+ {}".format(cmd))
+        subprocess.check_call(shlex.split(cmd))
+    sys.exit(0)
+
+
+setup_requires = []
+install_requires = [
+    'cameramodels',
+    'gdown',
+    'pybullet',
+    'scikit-robot',
+]
+
+
+setup(
+    name="hanging_points_cnn",
+    version=version,
+    description="hanging points cnn",
+    author="kosuke55",
+    author_email="kosuke.tnp@gmail.com",
+    url="https://github.com/kosuke55/hanging_points_cnn",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+    ],
+    packages=find_packages(),
+    zip_safe=False,
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+)
