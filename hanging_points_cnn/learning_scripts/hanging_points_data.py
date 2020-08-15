8605f7792c05e8a9534c9f6a76436477198ca4c3
kosuke55 <kosuke.tnp@gmail.com>
2020-08-15 16:40:50 +0900

    Rename test -> val

--- hanging_points_cnn/learning_scripts/hanging_points_data.py
+++ hanging_points_cnn/learning_scripts/hanging_points_data.py
@@ -29,18 +29,17 @@ def load_dataset(data_path, batch_size, use_bgr, use_bgr2gray, depth_range):
         data_path, transform, use_bgr, use_bgr2gray, depth_range)
 
     train_size = int(0.9 * len(hp_data))
-    test_size = len(hp_data) - train_size
-    # train_size = 1
-    # test_size = 1
-    train_dataset, test_dataset = random_split(
-        hp_data, [train_size, test_size])
+    val_size = len(hp_data) - train_size
+
+    train_dataset, val_dataset = random_split(
+        hp_data, [train_size, val_size])
 
     train_dataloader = DataLoader(
         train_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
-    test_dataloader = DataLoader(
-        test_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
+    val_dataloader = DataLoader(
+        val_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
 
-    return train_dataloader, test_dataloader
+    return train_dataloader, val_dataloader
 
 
 class HangingPointsDataset(Dataset):
