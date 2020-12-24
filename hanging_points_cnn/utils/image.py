f8c344120f0db3602cbd837aed2ea7e583353f85
kosuke55 <kosuke.tnp@gmail.com>
2020-12-25 00:37:47 +0900

    Add depth augmentation to image utils

--- hanging_points_cnn/utils/image.py
+++ hanging_points_cnn/utils/image.py
@@ -6,6 +6,8 @@ import sys
 from PIL import Image
 
 import numpy as np
+import imgaug.augmenters as iaa
+from hanging_points_cnn.utils.random_eraser import get_random_eraser
 
 try:
     import cv2
@@ -359,3 +361,45 @@ def overlay_heatmap(image, heatmap):
     heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
     added_image = cv2.addWeighted(image, 0.3, heatmap, 0.7, 0)
     return added_image
+
+
+def augment_depth(depth, random_value=False):
+    """data augmentation for depth image
+
+    Parameters
+    ----------
+    depth : numpy.ndarray
+    random_value : bool, optional
+        if true, erase with pixel level random value,
+        by default False
+
+    Returns
+    -------
+    depth : numpy.ndarray
+    """
+    aug_seq = iaa.Sequential([
+        iaa.Dropout([0, 0.8]),
+        iaa.MultiplyElementwise((0.99, 1.01)),
+    ], random_order=True)
+
+    depth = depth_edges_erase(depth)
+    depth = aug_seq.augment_image(depth)
+
+    nonzero_depth = depth.copy()
+    nonzero_depth[nonzero_depth == 0] = depth.max()
+
+    if random_value:
+        depth_eraser = get_random_eraser(
+            p=0.9, s_l=0.1, s_h=0.5,
+            v_l=nonzero_depth.min(),
+            v_h=depth.max(),
+            pixel_level=False)
+    else:
+        depth_eraser = get_random_eraser(
+            p=0.9, s_l=0.1, s_h=0.5,
+            v_l=0,
+            v_h=0,
+            pixel_level=False)
+    depth = depth_eraser(depth)
+
+    return depth
