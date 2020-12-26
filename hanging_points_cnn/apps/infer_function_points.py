ab1f7e066eaa9801e22c9408974fa8202a86bcaf
kosuke55 <kosuke.tnp@gmail.com>
2020-12-26 18:48:01 +0900

    Load pretrained model with strict=False

--- hanging_points_cnn/apps/infer_function_points.py
+++ hanging_points_cnn/apps/infer_function_points.py
@@ -103,7 +103,7 @@ def main():
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = HPNET(config).to(device)
-    model.load_state_dict(torch.load(pretrained_model))
+    model.load_state_dict(torch.load(pretrained_model), strict=False)
     model.eval()
 
     viewer = skrobot.viewers.TrimeshSceneViewer(resolution=(640, 480))
